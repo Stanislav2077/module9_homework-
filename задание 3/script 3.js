a28b24e71f0d662fbfc30ec4f210ce233957cb32
StasLis77 <u-stasa77@rambler.ru>
2022-12-21 18:08:22 +0300

    task 3

--- задание 3/script 3.js
+++ задание 3/script 3.js
@@ -1,61 +1,65 @@
-//URL-путь к JSON-файлу
-const reqUrl = "https://my.api.mockaroo.com/revenue_2017-2019.json?key=fd36b440";
-
-//кнопка
-const btnGet = document.querySelector('.getList');
-
-//список с годами
-const yearList = document.querySelector('.year');
-
-//первая строка таблицы
-const tableFirstLine = document.querySelector('.year_tr_first');
-
-//вторая строка таблицы
-const tableSecondLine = document.querySelector('.year_tr_second');
-
-//таблица
-function DrawTable(yearData) {
-	let firstLine = '';
-	let secondLine = '';
-
-	for (var key in yearData.sales) {
-		console.log(key);
-		console.log(yearData.sales[key]);
-		const yearCell = `<td>${key}</td>`;
-		const yearDataCell = `<td>${yearData.sales[key]}</td>`;
-		firstLine = firstLine + yearCell;
-		secondLine = secondLine + yearDataCell;
-	}
-
-	tableFirstLine.innerHTML = firstLine;
-	tableSecondLine.innerHTML = secondLine;
-}
-
-//получение данных по выбранному году
-function GetYearData(year, yearJSON) {
-	const years = JSON.parse(yearJSON);
-	for (var i = 0; i < years.length; i++) {
-		var currentYear = years[i];
-		if (currentYear.year == year) {
-			DrawTable(currentYear);
+//Функция-обертка над XMLHttpRequest, осуществляющая запрос
+   //url 
+ 
+function useRequest(url, callback) {
+var xhr = new XMLHttpRequest();
+xhr.open('GET', url, true);
+	
+xhr.onload = function() {
+	if (xhr.status != 200) {
+	console.log('Статус ответа: ', xhr.status);
+	} else {
+		const result = JSON.parse(xhr.response);
+		
+		if (callback) {
+		callback(result);
 		}
 	}
+
+};
+	
+	
+xhr.onerror = function() {
+	console.log('Ошибка! Статус ответа: ', xhr.status);
+};
+	
+xhr.send();
+}; 
+
+  // Ищем ноду для вставки результата запроса
+const resultNode = document.querySelector('.error');
+  // Ищем кнопку, по нажатии на которую будет запрос
+const btnNode = document.querySelector('.btn-request');
+const value = document.querySelector('input');
+
+
+
+
+	// Функция обработки полученного результата
+	// apiData - объект с результатом запроса
+	
+function displayResult(apiData) {
+let cards = '';
+	// console.log('start cards', cards);
+	
+
+	if( ! Number(value.value) || Number(value.value)  > 10 || Number(value.value) < 1 ) {
+		resultNode.innerHTML = 'Число вне диапазона от 1 до 10';
+	}else {
+        apiData.forEach(item => {
+			const cardBlock = `
+				<div class="card">
+				<img src="${item.download_url}" class="card-image"/>
+				<p>${item.author}</p>
+			</div>
+			`;
+			cards = cards + cardBlock;
+		});
+		resultNode.innerHTML = cards;
+	}
 }
 
-//получение данных
-function GetData(year, callback) {
-	var xhr = new XMLHttpRequest();
-	xhr.open('GET', reqUrl);
-	xhr.send();
-	xhr.onerror = function () {
-		console.log('Ошибка запроса');
-	};
-	xhr.onload = function () {
-		callback(year, xhr.response);
-	};
-}
-
-//кнопка с обработчиком события
-btnGet.addEventListener('click', () => {
-	GetData(yearList.value, GetYearData);
+  // Вешаем обработчик на кнопку для запроса
+btnNode.addEventListener('click', () => {
+	useRequest(`https://picsum.photos/v2/list/?limit=${parseInt(value.value)}`, displayResult);
 });
