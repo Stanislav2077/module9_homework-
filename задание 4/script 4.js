2dc47be2a7d1ce002c1f8039476be7dc7c895ff6
StasLis77 <u-stasa77@rambler.ru>
2022-12-22 13:52:22 +0300

    task 4

--- задание 4/script 4.js
+++ задание 4/script 4.js
@@ -1,81 +1,44 @@
-//Функция-обертка над XMLHttpRequest
- 
-function useRequest1(url, callback) {
-	var xhr = new XMLHttpRequest();
-	xhr.open('GET', url, true);
-	
-	xhr.onload = function() {
-	  if (xhr.status != 200) {
-		console.log('Статус ответа: ', xhr.status);
-	  } else {
-		const result = JSON.parse(xhr.response);
-		
-		if (callback) {
-		  callback(result);
-		}
-	  }
-	  
-	};
-	
-	xhr.onerror = function() {
-	  console.log('Ошибка! Статус ответа: ', xhr.status);
-	};
-	
-	xhr.send();
-  }; 
-  
-  // Ищем ноду для вставки результата запроса
-  const resultNode1 = document.querySelector('.error1');
-  const resultNode2 = document.querySelector('.error2');
-  const resultNode3 = document.querySelector('.error3');
-  // Ищем кнопку, по нажатии на которую будет запрос
-  const btnNode1 = document.querySelector('.btn-request1');
-  const input1 = document.querySelector('.input1');
-  const input2 = document.querySelector('.input2');
-  
-  
-  
-  
-	// Функция обработки полученного результата
-	// apiData - объект с результатом запроса
-	
-   function displayResult1(apiData) {
-	let cards = '';
-	// console.log('start cards', cards);
-	
+function pageLoad() {
+	resultNode = document.querySelector('.result');
+	const btnNode = document.querySelector('.btn');
 
-	if( ! Number(input1.value) || Number(input1.value)  > 300 || Number(input1.value) < 100 ) {
-		
-		resultNode1.innerHTML = 'Число вне диапазона от 100 до 300';
-		setTimeout(() => {
-			resultNode1.innerHTML = ' ';
-		}, 3000);
-	
-	}else if(! Number(input2.value) || Number(input2.value)  > 300 || Number(input2.value) < 100) {	
-		    
-		resultNode2.innerHTML = 'Число вне диапазона от 100 до 300';
-		setTimeout(() => {
-			resultNode2.innerHTML = ' ';
-		}, 3000);
-	
-	}else {
-		apiData.forEach(item => {
-		const cardBlock = `
-			  <div class="card">
-				<img
-				  src="${item.file}"
-				  class="card-image"
-				/>
-			  </div>
-			`;
-			cards = cards + cardBlock;
-		});
-		  
-		  resultNode3.innerHTML = cards;
-	};
-  }
-  
-  // Вешаем обработчик на кнопку для запроса
-  btnNode1.addEventListener('click', () => {
-	useRequest1(`https://loremflickr.com/json/g/${input1.value}/${input2.value}/all`, displayResult1);
-  });
+btnNode.addEventListener('click', () => {
+	input1Node = Math.round(document.querySelector('.input1').value);
+	input2Node = Math.round(document.querySelector('.input2').value);
+	if (input1Node >= 100 && input1Node <= 500 
+		&& input2Node >= 100 && input2Node <= 500) {
+		let url = `https://picsum.photos/${input1Node}/${input2Node}`;
+		useRequest(url);
+	}
+	else {
+		const error = `<p>Oдно из чисел вне диапазона от 100 до 500</p>`;
+		resultNode.innerHTML = error;
+	}
+});
+}
+
+function useRequest(url) {
+	console.log(url);
+	fetch(url)
+	.then((response) => { 
+	const result = response.url;
+	console.log('result', result);
+	return result;
+	})
+	.then((data) => { 
+		console.log('data', data);
+		let cardBlock = `<div class="card">
+			<img
+			src="${data}" Width="${input1Node}" Height="${input2Node}"
+			class="card-image"/>
+		</div>`;
+	console.log('cardBlock', cardBlock);
+	resultNode.innerHTML = cardBlock;
+	})
+	.catch(() => { console.log('Ошибка!') });  
+};
+
+let resultNode;
+let input1Node;
+let input2Node;
+document.addEventListener("DOMContentLoaded", pageLoad);
